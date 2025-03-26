4e18a10e5335ad3e3a1307411a3032e6e3e10fb1
piotr <piotrusu@gmail.com>
2025-03-26 16:46:42 +0100

    test project

--- ProjectTest/UnitTest1.cs
+++ ProjectTest/UnitTest1.cs
@@ -13,9 +13,23 @@ using Serilog;
 using Microsoft.AspNetCore.Hosting;
 using ZadanieMFV.Logic;
 using ZadanieMFV.Models;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.Extensions.Logging;
 
 namespace ProjectTest
 {
+
+    /*
+     * Troche poszalalem . Ale chodzi o to zeby dynamicznie ladowac zaleznosci oraz  testowac funkcje bezposrednio  a nie przez request . 
+     * Ladujemy zaleznosci poczym  budujemy model oraz uruchamiamy funkcje 
+     * jest to troche inne podejscie niz  zazwyczaj 
+     * Pozwala to testowac poza warstwa prezentacji . 
+     * Oczywiscie w przypadku endpointow  jest to moze podejscie  na okolo  no bo prosciej przepytac api . Ale takie podejscie 
+     * pozwala na czastkowe testowanie funkcji nie zaleznie 
+     * A w przypadku aplikacji ktore nie posiadaja endpointow ( na przyklad blazor )  pozwala na ladowanie zaleznosci i testowanie 
+     * W tym przypadku uzylem  tylko jednej klasy ale tak naprawde mozna zaladowac calosc 
+     * 
+     */
     public class Tests : IAsyncLifetime, IDisposable
     {
        
@@ -23,59 +37,51 @@ namespace ProjectTest
 
         public IManagedMqttClient MqttClient { get; private set; }
         public IHost Server { get; private set; }
-
+        
         public Bunit.TestContext IndexPage { get; } // TODO : dispose
 
         public Tests()
         {
 
             // load secrets from .env file, or load them from env vars if running in a docker container
-            //EnvironmentHelper.LoadEnvironmentVariables();
+           // EnvironmentHelper.LoadEnvironmentVariables();
 
             // based upon the ASPNETCORE_ENVIRONMENT get the config meta data
             // ConfigMetaData = SettingsFile.GetConfigMetaData();
 
             var config = InitConfiguration();
-            //zeby zaladowac zmienne srodowiskowe 
-            //ladujemy wszystkie zaleznosci , tak prosciej niz ladowac  potrzebne nam tylko . 
-            //Nalezy pamietac ze korzystamy z appsettings i uruchamia nam sie jako production.
            
 
-            //var mockApplicationPrincipal = new Mock<IApplicationPrincipal>();
-            //mockApplicationPrincipal.Setup(ap => ap.Id).Returns(1);
-            //mockApplicationPrincipal.Setup(ap => ap.CurrentContextFacilityId).Returns(7);
 
-            //var appPrincipal = new TestApplicationPrincipal(null);
 
             string[] args = new string[0];
             // build the host instance
-            Server = (IHost?)Host.CreateDefaultBuilder(args)
-                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-                .UseConsoleLifetime()
-                .UseSerilog((hostingContext, services, loggerConfiguration) => loggerConfiguration
-                    .ReadFrom.Configuration(hostingContext.Configuration)
-                    .Enrich.FromLogContext())
-            .ConfigureWebHost(webBuilder =>
-                {
-                    webBuilder
-                     .UseKestrel()
-                    .UseUrls("http://127.0.0.1:5000"); // TODO: workout how to make dynamic and reference with NavigationManager
-
-
-                })
-                .ConfigureServices((hostContext, service) =>
-                {
-                    
-                })
-                .ConfigureContainer<ContainerBuilder>(builder =>
-                {
-                }).Build();
-
-            
+           
+            Server = new HostBuilder()
+               .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+               .ConfigureLogging(logging =>
+               {
+                   logging.ClearProviders();
+                   logging.AddConsole();
+                   logging.AddFilter("Microsoft.AspNetCore.SignalR", LogLevel.Information);
+               })
+               .ConfigureWebHost(webBuilder =>
+               {
+                   webBuilder
+                    //.UseStartup<Startup>()
+                   .UseKestrel()
+                   
+                   .UseUrls("https://127.0.0.1:5000"); // TODO: workout how to make dynamic and reference with NavigationManager
+                   
+               })
+               .ConfigureServices((hostContext, service) => {
+                   service.AddScoped<SendMail>();
+              })
+               .Build();
         }
         private void InitialisePageDependencies()
         {
-
+            
         }
 
         public static IConfiguration InitConfiguration()
@@ -91,6 +97,7 @@ namespace ProjectTest
         [SetUp]
         public void Setup()
         {
+            InitialisePageDependencies();
         }
 
         [Test]
@@ -109,20 +116,20 @@ namespace ProjectTest
         public async Task InitializeAsync()
         {
             //Poniewaz nie uruchamiamy tylko ladujemy zaleznosci nie ma potrzeby odpalac aplikacji 
-            //try
-            //{
+            try
+            {
 
-            //    await Server.RunAsync();
-            //    await Server.StartAsync();
+                await Server.RunAsync();
+                await Server.StartAsync();
 
-            //    //  MqttClient = (Server.Services.GetService(typeof(IMqttService)) as IMqttService).Client;
+                //  MqttClient = (Server.Services.GetService(typeof(IMqttService)) as IMqttService).Client;
 
-            //    InitialisePageDependencies();
-            //}
-            //catch (Exception ex)
-            //{ 
-            //    var t =ex.ToString();
-            //}
+                
+            }
+            catch (Exception ex)
+            {
+                var t = ex.ToString();
+            }
         }
         public async Task DisposeAsync()
         {
